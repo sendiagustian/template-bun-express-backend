a362a76dbbddb2ff23c51ad5b70ad13ce3672cdc
sendiagustian <sendiagustian.pk@gmail.com>
2024-08-31 21:51:06 +0700

    update config tsscript and eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,12 +3,7 @@ import { parser as typescriptParser } from "typescript-eslint";
 
 export default {
     files: ["**/*.{js,mjs,cjs,ts}"],
-    languageOptions: { globals: globals.node },
-    languageOptions: {
-        parser: typescriptParser,
-        ecmaVersion: 2020,
-        sourceType: "commonjs"
-    },
+    languageOptions: { globals: globals.node, parser: typescriptParser, ecmaVersion: 2020, sourceType: "commonjs" },
     rules: {
         strict: "off",
         indent: ["error", 4],
